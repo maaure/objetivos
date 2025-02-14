21d667df8ab0bba8948284cf01055bac8da45bf5
Maure Andrade <pedro.maure@gmail.com>
2025-02-14 19:52:52 -0300

    feat: add utility functions file

---
+++ src/lib/utils.ts
@@ -0,0 +1,17 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Delivery, OKR } from "./definitions";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function calculateProgress(deliveries: Delivery[]) {
+  if (!deliveries.length) return 0;
+  return Math.ceil(deliveries.reduce((sum, d) => sum + Number(d.value), 0) / deliveries.length);
+}
+
+export function calculateObjetivoProgress(okr: OKR) {
+  const total = okr.resultKeys.reduce((sum, rk) => sum + calculateProgress(rk.deliveries), 0);
+  return Math.ceil(total / okr.resultKeys.length);
+}
