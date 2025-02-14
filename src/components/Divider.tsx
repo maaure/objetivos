d652ada62be66b597cef3027af74e37531dda3de
Maure Andrade <pedro.maure@gmail.com>
2025-02-14 19:52:20 -0300

    feat: add className prop to Divider component

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,17 +1,18 @@
 interface DividerProps {
   children?: React.ReactNode;
+  className?: string;
 }
 
-export const Divider = ({ children }: DividerProps) => {
+export const Divider = ({ children, className }: DividerProps) => {
   if (!children) {
-    return <div className="border-t border-gray-300 my-4"></div>;
+    return <div className={`border-t border-gray-300 ${className}`} />;
   }
 
   return (
-    <div className="relative flex items-center py-4">
-      <div className="flex-grow border-t border-gray-300 md:block"></div>
+    <div className={`relative flex items-center ${className}`}>
+      <div className="flex-grow border-t border-gray-300 md:block" />
       <span className="mx-4 flex-shrink text-sm text-gray-500 font-medium">{children}</span>
-      <div className="flex-grow border-t border-gray-300 hidden md:block"></div>
+      <div className="flex-grow border-t border-gray-300 hidden md:block" />
     </div>
   );
 };
