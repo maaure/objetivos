8ac1318951029b9351f302534afd2bdc674f388e
Maure Andrade <pedro.maure@gmail.com>
2025-02-15 17:23:53 -0300

    feat: implements api funcitons

--- src/components/forms/ResultadoChaveForm.tsx
+++ src/components/forms/ResultadoChaveForm.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
+
 import * as yup from "yup";
-import { Input } from "../ui/input";
+
 import {
   Form,
   FormControl,
@@ -10,22 +9,26 @@ import {
   FormItem,
   FormMessage,
 } from "../ui/form";
-import { Button } from "../ui/button";
-import { useFieldArray } from "react-hook-form";
 import { Plus, Trash } from "lucide-react";
+import { createResultsKey, updateResultsKey } from "@/lib/api";
+
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 import { Results } from "@/lib/definitions";
+import { useFieldArray } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const formSchema = yup.object({
-  resultado_chave: yup
-    .string()
-    .required("O nome do resultado-chave é obrigatório"),
-  entregas: yup
+  name: yup.string().required("O nome do resultado-chave é obrigatório"),
+  deliveries: yup
     .array()
     .of(
       yup.object().shape({
-        name: yup.string(),
+        name: yup.string().required(),
         value: yup
           .number()
+          .required()
           .moreThan(0, "O valor deve ser maior que zero")
           .typeError("O valor deve ser um número"),
       })
@@ -36,18 +39,22 @@ const formSchema = yup.object({
 type FormValues = yup.InferType<typeof formSchema>;
 
 interface ResultadoChaveFormProps {
-  data?: Results;
+  okrId?: number;
+  result?: Results;
 }
 
-export const ResultadoChaveForm = ({ data }: ResultadoChaveFormProps) => {
+export const ResultadoChaveForm = ({
+  okrId: id,
+  result,
+}: ResultadoChaveFormProps) => {
   const form = useForm<FormValues>({
     resolver: yupResolver(formSchema),
     defaultValues: {
-      resultado_chave: data?.name,
-      entregas: data?.deliveries ?? [
+      name: result?.name,
+      deliveries: result?.deliveries ?? [
         {
-          name: undefined,
-          value: undefined,
+          name: "",
+          value: 0,
         },
       ],
     },
@@ -55,11 +62,15 @@ export const ResultadoChaveForm = ({ data }: ResultadoChaveFormProps) => {
 
   const { fields, append, remove } = useFieldArray({
     control: form.control,
-    name: "entregas",
+    name: "deliveries",
   });
 
   function onSubmit(data: FormValues) {
-    console.log(data);
+    if (result?.id) {
+      updateResultsKey(result, data);
+    } else if (id) {
+      createResultsKey(id, data);
+    }
   }
 
   return (
@@ -67,23 +78,17 @@ export const ResultadoChaveForm = ({ data }: ResultadoChaveFormProps) => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 pt-4">
         <FormField
           control={form.control}
-          name="resultado_chave"
+          name="name"
           render={({ field }) => (
             <FormItem>
               <FormControl>
                 <Input
                   placeholder="Digite o Resultado-Chave"
                   {...field}
-                  className={
-                    form.formState.errors.resultado_chave
-                      ? "border-red-500"
-                      : ""
-                  }
+                  className={form.formState.errors.name ? "border-red-500" : ""}
                 />
               </FormControl>
-              <FormMessage>
-                {form.formState.errors.resultado_chave?.message}
-              </FormMessage>
+              <FormMessage>{form.formState.errors.name?.message}</FormMessage>
             </FormItem>
           )}
         />
@@ -94,7 +99,7 @@ export const ResultadoChaveForm = ({ data }: ResultadoChaveFormProps) => {
               <div className="flex-1 grid grid-cols-2 gap-4">
                 <FormField
                   control={form.control}
-                  name={`entregas.${index}.name`}
+                  name={`deliveries.${index}.name`}
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
@@ -102,21 +107,24 @@ export const ResultadoChaveForm = ({ data }: ResultadoChaveFormProps) => {
                           placeholder="Digite a entrega"
                           {...field}
                           className={
-                            form.formState.errors.entregas?.[index]?.name
+                            form.formState.errors.deliveries?.[index]?.name
                               ? "border-red-500"
                               : ""
                           }
                         />
                       </FormControl>
                       <FormMessage>
-                        {form.formState.errors.entregas?.[index]?.name?.message}
+                        {
+                          form.formState.errors.deliveries?.[index]?.name
+                            ?.message
+                        }
                       </FormMessage>
                     </FormItem>
                   )}
                 />
                 <FormField
                   control={form.control}
-                  name={`entregas.${index}.value`}
+                  name={`deliveries.${index}.value`}
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
@@ -125,7 +133,7 @@ export const ResultadoChaveForm = ({ data }: ResultadoChaveFormProps) => {
                           type="number"
                           {...field}
                           className={
-                            form.formState.errors.entregas?.[index]?.name
+                            form.formState.errors.deliveries?.[index]?.name
                               ? "border-red-500"
                               : ""
                           }
@@ -133,7 +141,7 @@ export const ResultadoChaveForm = ({ data }: ResultadoChaveFormProps) => {
                       </FormControl>
                       <FormMessage>
                         {
-                          form.formState.errors.entregas?.[index]?.value
+                          form.formState.errors.deliveries?.[index]?.value
                             ?.message
                         }
                       </FormMessage>
@@ -155,7 +163,7 @@ export const ResultadoChaveForm = ({ data }: ResultadoChaveFormProps) => {
             <Button
               type="button"
               variant="ghost"
-              onClick={() => append({})}
+              onClick={() => append({ name: "", value: 0 })}
               className="place-items-end self-end"
             >
               <Plus /> Adicionar Entrega
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,7 @@
+import { Delivery, Objetivo, Results } from "./definitions";
+
 import axios from "axios";
-import { Objetivo } from "./definitions";
+
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const axiosInstance = axios.create({
@@ -16,5 +18,24 @@ export async function createOKR(
   data: Pick<Objetivo, "name">
 ): Promise<Objetivo> {
   const response = await axiosInstance.post("okrs", data);
+  return response.data;
+}
+
+export async function createResultsKey(
+  id: number,
+  data: Pick<Results, "name" | "deliveries">
+): Promise<Delivery> {
+  const response = await axiosInstance.post(`okrs/${id}/resultKeys`, data);
+  return response.data;
+}
+
+export async function updateResultsKey(
+  result: Results,
+  data: Pick<Results, "name" | "deliveries">
+): Promise<Delivery> {
+  const response = await axiosInstance.post(
+    `okrs/${result.okrId}/resultKeys/${result.id}`,
+    data
+  );
   return response.data;
 }
