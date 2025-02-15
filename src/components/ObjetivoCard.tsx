7ac2288674030dd55d585638784518f5b880303b
Maure Andrade <pedro.maure@gmail.com>
2025-02-15 18:06:25 -0300

    feat: add loading state and skeleton for results in ObjetivoCard

--- src/components/ObjetivoCard.tsx
+++ src/components/ObjetivoCard.tsx
@@ -1,17 +1,38 @@
+"use client";
+
 import { Divider } from "./ui/divider";
-import { Objetivo } from "@/lib/definitions";
+import { Objetivo, Results } from "@/lib/definitions";
 import OpenDialog from "./OpenDialog";
 import { ProgressBar } from "./ProgressBar";
 import { ResultadoChaveForm } from "./forms/ResultadoChaveForm";
 import ResultadoSection from "./ResultadoSection";
 import { calculateObjetivoProgress } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { getResultsKeysByOKRId } from "@/lib/api";
+import { Skeleton } from "./ui/skeleton";
 
 export interface ObjetivoCardProps {
   okr: Objetivo;
 }
 
 export const ObjetivoCard = ({ okr }: ObjetivoCardProps) => {
-  const { name, resultKeys } = okr;
+  const { name } = okr;
+  const [resultKeys, setResultKeys] = useState<Results[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getResultsKeysByOKRId(okr.id)
+      .then((data) => {
+        setResultKeys(data);
+      })
+      .catch(() => {
+        setResultKeys([]);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [okr]);
 
   return (
     <div className="flex flex-col w-full gap-2">
@@ -22,12 +43,22 @@ export const ObjetivoCard = ({ okr }: ObjetivoCardProps) => {
         </div>
         <Divider> Resultados-Chave </Divider>
         <div className="p-6">
-          {resultKeys?.map((resultKey, index) => (
-            <div key={resultKey.id}>
-              <ResultadoSection resultado={resultKey} />
-              {index < resultKeys.length - 1 && <Divider className="my-4" />}
-            </div>
-          ))}
+          {isLoading
+            ? [...Array(2)].map((_, index) => (
+                <div key={index}>
+                  <div className="space-y-2 mb-4">
+                    <Skeleton className="h-4 w-[250px]" />
+                    <Skeleton className="h-3 w-[200px]" />
+                  </div>
+                  {index < 1 && <Divider className="my-4" />}
+                </div>
+              ))
+            : resultKeys.map((resultKey, index) => (
+                <div key={resultKey.id}>
+                  <ResultadoSection resultado={resultKey} />
+                  {index < resultKeys.length - 1 && <Divider className="my-4" />}
+                </div>
+              ))}
         </div>
       </div>
       <OpenDialog
